a4d9c05db5a775bd7d338cfd4ff9cd7c63907d4f
ricardochampi <brayanchampi@gmail.com>
2021-12-30 11:58:03 -0500

    homework de 04 a 07

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -90,7 +90,11 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  return num > 50
+  if(num > 50){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function obtenerResto(x, y) {
